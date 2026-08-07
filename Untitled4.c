95c3bf1dde45bc833f4489e039513fc6b9918030
om23getit <ompatel5062@gmail.com>
2026-08-07 10:45:47 +0530

    Add files via upload

---
+++ Untitled4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c,d,e,f,g,h;
+float i;
+a=18;
+b=18;
+c=8;
+d=8;
+e=23;
+f=23;
+g=8;
+h=8;
+i= sqrt((a*b)+(c*d)+(e*f)+(g*h));
+printf("a=");
+printf("\n Result of given problem will be %f",i);
+}
